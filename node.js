768bff22f534d29739c588141cf3c71d7479800b
Jason <jasonsenpai@gmail.com>
2019-02-11 23:44:17 -0800

    Update

--- node.js
+++ node.js
@@ -5,17 +5,35 @@ var nodes = [{x: 30, y: 50},
 var width = 900;
 var height = 900;
 
+
+
+
+
+
+var link = d3.select('svg').append("g")
+  .attr("class", "links")
+  .selectAll("line")
+ // .data(graph.links)
+ // .enter().append("line")
+  .attr("stroke-width", 50);
+
+
 var node_svg = d3.select('#psl_graph').append('svg');
-
-
 
 node_svg.attr('width', width)
 		.attr('height', height);
 
-node_svg.selectAll("g")
+node_svg.append("g")
+		.attr('class', 'node')
+		.selectAll('circle')
     	.data(nodes)
      	.enter()
-    	.append("svg:circle")
-     	.attr("class", "nodes")
+    	.append("circle")
+    	.attr('r', function(d,i){return 10 + 10*i})
      	.attr("cx", function(d) { return d.x; })
      	.attr("cy", function(d) { return d.y; })
+
+
+
+// Made for dragging?
+     	// .call(d3.drag().on("drag", mousemove));
