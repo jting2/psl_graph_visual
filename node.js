352b544692a67431c4a6b468cea798995109b75f
Jason <jasonsenpai@gmail.com>
2019-02-15 17:24:46 -0800

    Update

--- node.js
+++ node.js
@@ -1,55 +1,61 @@
-var nodes = [{x: 30, y: 50},
-              {x: 50, y: 80},
-              {x: 90, y: 120}]
-
-var links = [
-  {source: nodes[0], target: nodes[1]},
-  {source: nodes[2], target: nodes[1]}
-]
-
-var width = 900;
-var height = 900;
-
-
-
-d3.csv('data/globallikes_obs_atom.csv', function(error, data) {
-	console.log(data[0])
-   if (error) throw error;
+$(document).ready(function() {
+    main();
 });
 
+function main() {
+	var nodes = window.pslviz.nodes;
 
-// var link = d3.select('svg').append("g")
-//   .attr("class", "links")
-//   .selectAll("line")
-//  // .data(graph.links)
-//  // .enter().append("line")
-//   .attr("stroke-width", 50);
+	var links = [
+	  {source: nodes[0], target: nodes[1]},
+	  {source: nodes[2], target: nodes[1]}
+	]
 
-
-var node_svg = d3.select('#psl_graph').append('svg');
-
-node_svg.attr('width', width)
-		.attr('height', height);
-
-node_svg.append('g').selectAll('circle .nodes')
-    	.data(nodes)
-     	.enter()
-    	.append("circle")
-		.attr('class', 'node')
-    	.attr('r', 10)
-     	.attr("cx", function(d) { return d.x; })
-     	.attr("cy", function(d) { return d.y; });
+	var width = 900;
+	var height = 900;
 
 
-node_svg.selectAll('.line')
-		.data(links)
-		.enter()
-		.append('line')
-		.attr("x1", function(d) { return d.source.x })
-   		.attr("y1", function(d) { return d.source.y })
-   		.attr("x2", function(d) { return d.target.x })
-   		.attr("y2", function(d) { return d.target.y })
-   		.style("stroke", "rgb(6,120,155)");
 
-// Made for dragging?
-     	// .call(d3.drag().on("drag", mousemove));
+	// d3.csv('data/globallikes_obs_atom.csv', function(error, data) {
+	// 	console.log(data[0])
+	//    if (error) throw error;
+	// });
+
+
+	// var link = d3.select('svg').append("g")
+	//   .attr("class", "links")
+	//   .selectAll("line")
+	//  // .data(graph.links)
+	//  // .enter().append("line")
+	//   .attr("stroke-width", 50);
+
+
+
+
+	var node_svg = d3.select('#psl_graph').append('svg');
+
+	node_svg.attr('width', width)
+			.attr('height', height);
+
+	node_svg.append('g').selectAll('circle .nodes')
+	    	.data(nodes)
+	     	.enter()
+	    	.append("circle")
+			.attr('class', 'node')
+	    	.attr('r', 10)
+	     	.attr("cx", function(d) { return 10 * d.x; })
+	     	.attr("cy", function(d) { return 10 * d.y; });
+
+
+	node_svg.selectAll('.line')
+			.data(links)
+			.enter()
+			.append('line')
+			.attr("x1", function(d) { return d.source.x })
+	   		.attr("y1", function(d) { return d.source.y })
+	   		.attr("x2", function(d) { return d.target.x })
+	   		.attr("y2", function(d) { return d.target.y })
+	   		.style("stroke", "rgb(6,120,155)");
+
+	// Made for dragging?
+	     	// .call(d3.drag().on("drag", mousemove));
+}
